6ac5d6dc216a35f728ed28f36667ed2bb9b623b9
ladianchad <qhrejddlvltm@gmail.com>
2021-06-19 20:19:53 +0900

    pt update

--- client_camera.py
+++ client_camera.py
@@ -6,8 +6,8 @@ import random
 
 from socket_funcs import *
 
-cam=cv2.VideoCapture('../churo2/dog_video/to_slice/running/normal7.avi') #cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FPS,20)
+cam=cv2.VideoCapture('../churo2/dog_video/to_slice/running/normal5.avi') #cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FPS,60)
 _,img=cam.read()
 
 
